12efebfee05ed768f706f22a1a766694b8779955
nakvi <stnaqvi9@gmail.com>
2024-07-24 21:16:52 +0500

    first commit

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,84 @@
+<!-- ========== App Menu ========== -->
+<div class="app-menu navbar-menu">
+    <!-- LOGO -->
+    <div class="navbar-brand-box">
+        <!-- Dark Logo-->
+        <a href="index" class="logo logo-dark">
+            <span class="logo-sm">
+                <img src="{{ URL::asset('build/images/logo-sm.png') }}" alt="" height="22">
+            </span>
+            <span class="logo-lg">
+                <img src="{{ URL::asset('build/images/logo-dark.png') }}" alt="" height="17">
+            </span>
+        </a>
+        <!-- Light Logo-->
+        <a href="index" class="logo logo-light">
+            <span class="logo-sm">
+                <img src="{{ URL::asset('build/images/logo-sm.png') }}" alt="" height="22">
+            </span>
+            <span class="logo-lg">
+                <img src="{{ URL::asset('build/images/logo-light.png') }}" alt="" height="17">
+            </span>
+        </a>
+        <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover" id="vertical-hover">
+            <i class="ri-record-circle-line"></i>
+        </button>
+    </div>
+
+    <div id="scrollbar">
+        <div class="container-fluid">
+
+            <div id="two-column-menu">
+            </div>
+            <ul class="navbar-nav" id="navbar-nav">
+                <li class="menu-title"><span>@lang('translation.menu')</span></li>
+                <li class="nav-item">
+                    <a class="nav-link menu-link" href="#sidebarDashboards" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarDashboards">
+                        <i class="ri-dashboard-2-line"></i> <span>@lang('translation.dashboards')</span>
+                    </a>
+                    <div class="collapse menu-dropdown" id="sidebarDashboards">
+                        <ul class="nav nav-sm flex-column">
+                            <li class="nav-item">
+                                <a href="dashboard-analytics" class="nav-link">@lang('translation.analytics')</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="dashboard-crm" class="nav-link">@lang('translation.crm')</a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </li> <!-- end Dashboard Menu -->
+
+
+                <li class="nav-item">
+                    <a class="nav-link menu-link" href="#sidebarApps" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarApps">
+                        <i class="ri-apps-2-line"></i> <span>@lang('translation.apps')</span>
+                    </a>
+                    <div class="collapse menu-dropdown" id="sidebarApps">
+                        <ul class="nav nav-sm flex-column">
+                            <li class="nav-item">
+                                <a href="apps-calendar" class="nav-link">@lang('translation.calendar')</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="apps-chat" class="nav-link">@lang('translation.chat')</a>
+                            </li>
+                           
+
+
+                            <li class="nav-item">
+                                <a href="apps-api-key" class="nav-link"> @lang('translation.api-key')</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+
+
+            </ul>
+        </div>
+        <!-- Sidebar -->
+    </div>
+    <div class="sidebar-background"></div>
+</div>
+<!-- Left Sidebar End -->
+<!-- Vertical Overlay-->
+<div class="vertical-overlay"></div>
